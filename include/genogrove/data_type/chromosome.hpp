d2ab28601cb97911ef2e1575e3d71e207c0e2497
riasc <richard.schaefer@zoho.com>
2025-04-22 23:19:08 -0500

    added class chromosome that stores encoded chromosome from the chromosome_registry

---
+++ include/genogrove/data_type/chromosome.hpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Indentifier: MIT
+ *
+ * Copyright (c) 2025 Richard A. Schäfer
+ *
+ * This file is part of genogrove and is licensed under the terms of the MIT license.
+ * See the LICENSE file in the root of the repository for more information.
+ */
+#ifndef DATATYPE_CHROMOSOME_HPP
+#define DATATYPE_CHROMOSOME_HPP
+
+// Standard
+
+// Class
+#include <genogrove/data_type/chromosome_registry.hpp>
+
+namespace genogrove::data_type {
+    class chromosome {
+        public:
+            chromosome(std::string name);
+            ~chromosome();
+
+            /*
+             * @ brief returns the name string of the chromosome
+             */
+            std::string get_name() const;
+
+        private:
+            uint8_t chromosome_index;
+    };
+}
+
+#endif //DATATYPE_CHROMOSOME_HPP
