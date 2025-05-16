c11ddf71b0562d5d3d26967faecf45f561ee3126
riasc <richard.schaefer@zoho.com>
2025-05-15 23:51:33 -0500

    added concepts implementation

--- include/genogrove/data_type/key_type_base.hpp
+++ include/genogrove/data_type/key_type_base.hpp
@@ -4,6 +4,7 @@
 #include <cstddef>
 
 #if __cplusplus >= 202002L
+// C++20 with concepts
 #include <concepts>
 
 namespace genogrove::data_type {
@@ -17,10 +18,21 @@ namespace genogrove::data_type {
         { a.getStart() } -> std::convertible_to<size_t>;
         { a.getEnd() } -> std::convertible_to<size_t>;
     };
+
+    // helper function for c++20 (with concept constraints)
+    template<key_type_base key_type>
+    constexpr bool is_key_type_base() {
+        return true;
+    }
 }
 
 #else
+// C++17 implementation (uses type traits and SFINAE)
 #include <type_traits>
+
+namespace genogrove::data_type {
+    
+}
 
 
 
