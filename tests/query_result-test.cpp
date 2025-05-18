e7badec51ec7d648333718750f8ce86211416239
riasc <richard.schaefer@zoho.com>
2025-05-17 23:25:01 -0500

    test queries

---
+++ tests/query_result-test.cpp
@@ -0,0 +1,21 @@
+/*
+ * SPDX-License-Indentifier: MIT
+ *
+ * Copyright (c) 2025 Richard A. Schäfer
+ *
+ * This file is part of genogrove and is licensed under the terms of the MIT license.
+ * See the LICENSE file in the root of the repository for more information.
+ */
+
+// Google Test
+#include <gtest/gtest.h>
+
+// Standard
+#include <string>
+
+// genogrove
+#include <genogrove/data_type/query_result.hpp>
+
+TEST(query_result_test, constructor) {
+    // Create a query result object, lets assume the query in an interval
+}
